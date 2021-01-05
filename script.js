fa841f2d6127fc95c14814c90a0423e7cda135fc
rahmadqolbi <75029066+rahmadqolbi@users.noreply.github.com>
2021-01-05 21:38:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+// alert('selamat pagi bos');
+// alert('Nama');
+// alert('saya');
+// alert('Rahmad Qolbi Syahrani');
+// alert('selamat pagi bos');
+// alert('Nama');
+// alert('saya');
+// alert('Rahmad Qolbi Syahrani');
+
+// alert memunculkan popup
+
+// var nama = prompt('masukkan nama kamu:');
+// alert(nama);
+
+// prompt
+
+// var tes = confirm('kamu yakin ingin keluar');
+// if( tes === true ){
+//     alert('user menekan ok !!!');
+// }
+// else {
+//     alert('user menekan cancel !!!');
+// }
+
+// confirm untuk meminta konfirmasi kepada user
+// diatas adalah pengkondisian
+
+// alert('selamat datang :)');
+// var lagi = true;
+// while( lagi === true ) {
+//     var nama = prompt('masukkan nama:');
+//     alert('Halo ' + nama);
+//     lagi = confirm('apakah anda yakin ingin keluar')
+//     alert('oke bang ' + nama + ' terima kasih..');
+// }
+alert('SELAMAT DATANG');
+var nama = prompt('Masukkan nama kamu :');{
+    if(nama) {
+        alert('Halo ' + nama + ' :)');
+    }else{
+        alert('Kamu belum isi nama oke gak masalah :)')
+    }
+}
+var tanya = prompt('Aku mau nanya sama ' + nama + ', udah save nomor WA Qolbi ?\nsudah atau belum');
+switch(tanya){
+    case 'belum':
+        alert('Kenapa gak di save ? :( ');
+        break;
+    case 'sudah':
+        alert('Terima Kasih :)' + nama);
+        break;
+        default:
+            alert('Anda memasukkan tulisan yang tidak sesuai huruf');
+            break;
+}
+
+
+
+
+
+
+
+
+
+
+// alert ('selamat pagi bang');
+// var lagi = true;
+// while(lagi) {
+//     var nama = prompt('masukkan nama anda');
+//     alert('halo bang ' + nama);
+//     lagi = confirm('yakin ingin keluar');
+// }
+// alert('terima kasih bang');
+
+
+// alert ('masukkan nama abang');
+// var lagi = true;
+
+// while(lagi === true) {
+//     var nama = prompt ('masukkan nama bang:');
+//     alert('Halo bang ' + nama);
+//     lagi = confirm('coba lagi bang')
+
+// }
+// alert('selesai bang');
+
+// alert ('selamat pagi admin');
+// var lagi=true;
+// while (lagi === true) {
+//     var nama = prompt ('masukkan nama anda');
+//     alert('Halo ' + nama);
+//     lagi = confirm('apakah anda mau keluar')
+// }
+// alert ('oke selesai bang')
+
+// pertemuan 1 WEB UNPAS
+
+
